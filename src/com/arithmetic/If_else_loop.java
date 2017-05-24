7092054a7ad5617bf50de41bd4bb214a16611a79
ishad <ishad@DESKTOP-GUULC2F.attlocal.net>
2017-05-23 22:37:33 -0400

    if_else_loop

---
+++ src/com/arithmetic/If_else_loop.java
@@ -0,0 +1,23 @@
+package com.arithmetic;
+
+public class If_else_loop {
+
+	public static void main(String[] args) {
+
+
+		int a = 5;
+		int b= 10;
+		if(a>b)
+		{
+			int c = a+ b;
+			System.out.println("value of a + b is :"+c);	
+
+		}
+		else
+		{
+			System.out.println("no value found");
+		}
+	}
+}
+
+
