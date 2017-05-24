b093d4aa931ff024f9414f7ba191766c98327b05
ishad <ishad@DESKTOP-GUULC2F.attlocal.net>
2017-05-23 22:29:08 -0400

    change value int a

--- src/com/arithmetic/Addition.java
+++ src/com/arithmetic/Addition.java
@@ -3,7 +3,7 @@ package com.arithmetic;
 public class Addition {
 
 	public static void main(String[] args) {
-		int a = 10;
+		int a = 20;
 		int b= 10;
 		int c = a+ b;
 		System.out.println("value of a + b is :"+c);
